7fd95399b7fdac24c335c4134fd1a82821888631
SuperAllexy <53560064+SuperAllexy@users.noreply.github.com>
2024-04-10 16:14:10 +0300

    Update AboutPage.js

--- src/routes/about/AboutPage.js
+++ src/routes/about/AboutPage.js
@@ -3,7 +3,6 @@ import Certifications from "./Certifications.js";
 import Languages from "./Languages.js";
 import Stack from "./Stack.js";
 import Education from "./Education.js";
-import WorkExperience from "./WorkExperience.js";
 import DownloadResume from "./DownloadResume.js";
 import AboutMe from "./AboutMe.js";
 import ScrollButton from "../../helpers/ScrollToTop";
